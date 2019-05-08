d681d862723deafab279c4af6d56986835722d8b
梁泽 <350442340@qq.com>
2019-05-08 14:12:40 +0800

    fix bug

--- JZTComponent/Tool/TableViewDataSource.swift
+++ JZTComponent/Tool/TableViewDataSource.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Foundation
-
+// zhao gf fix
 public class TableViewDataSource<Model, T: UITableViewCell>: NSObject, UITableViewDataSource {
     public typealias CellConfigurator = (Model, T) -> Void
     
