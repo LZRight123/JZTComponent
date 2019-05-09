d6507b66ff9037dc3741595f65c52e843358e8b7
梁泽 <350442340@qq.com>
2019-05-09 09:51:22 +0800

    1.0.4

--- JZTComponent.podspec
+++ JZTComponent.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = "JZTComponent"
-  s.version = "1.0.3"
+  s.version = "1.0.4"
   s.license = 'MIT'
   s.summary = "Categorys，tools，TransitioningAnimate."
   s.homepage = "https://github.com/LZRight123/JZTComponent.git"
@@ -10,20 +10,29 @@ Pod::Spec.new do |s|
     :tag => s.version,
     :submodules => true
   }
-
+  
   s.ios.deployment_target = '8.0'
   # s.osx.deployment_target = '10.12'
   # s.tvos.deployment_target = '10.0'
   # s.watchos.deployment_target = '3.0'
-
+  
   s.swift_version = '5.0'
   s.frameworks='Foundation','UIKit'
-  s.source_files  = "JZTComponent/{Categorys, Tool}/*.swift"
+  # s.source_files  = "JZTComponent/**/*.swift"
+  # s.source_files  = "JZTComponent/{Categorys, Tool}/*.swift"
   
   s.subspec 'Animator' do |an|
     an.source_files = 'JZTComponent/Animator/*.swift'
     # an.compiler_flags = '-Wno-incomplete-implementation -Wno-missing-prototypes'
     # an.dependency 'Alamofire'
+  end
+  
+  s.subspec 'Tool' do |tool|
+    tool.source_files = 'JZTComponent/Tool/*.swift'
+  end
+  
+  s.subspec 'Categorys' do |ca|
+    ca.source_files = 'JZTComponent/Categorys/*.swift'
   end
   
   s.subspec 'TransitioningAnimate' do |transitioningAnimate|
--- JZTComponent/Tool/LZToolBar.swift
+++ JZTComponent/Tool/LZToolBar.swift
@@ -17,8 +17,8 @@ public class LZToolBar: SpringView {
     }()
     private let backgroundImageView = UIImageView()
     
-    let safeAreaView = UIImageView()
-    var didTouchToolbar: (() -> Void)?
+    public let safeAreaView = UIImageView()
+    public var didTouchToolbar: (() -> Void)?
     
     override public var backgroundColor: UIColor?  {
         didSet{
@@ -26,26 +26,26 @@ public class LZToolBar: SpringView {
         }
     }
     
-    @IBInspectable var translucent = false {
+    @IBInspectable public var translucent = false {
         didSet{
             effectView.isHidden = !translucent
         }
     }
     
-    @IBInspectable var backgroundImage: UIImage? {
+    @IBInspectable public var backgroundImage: UIImage? {
         didSet{
             backgroundImageView.image = backgroundImage
         }
     }
     
-    @IBInspectable var effectSyle = UIBlurEffect.Style.extraLight {
+    @IBInspectable public var effectSyle = UIBlurEffect.Style.extraLight {
         didSet{
             effectView.effect = nil
             effectView.effect = UIBlurEffect(style: effectSyle)
         }
     }
     
-    override init(frame: CGRect) {
+    override public init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
@@ -65,14 +65,16 @@ public class LZToolBar: SpringView {
     override public func layoutSubviews() {
         super.layoutSubviews()
         
-        backgroundImageView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height + SafeBottomArea)
-        safeAreaView.frame = CGRect(x: 0, y: bounds.height, width: bounds.width, height: SafeBottomArea)
+        let statusBarH   = UIApplication.shared.statusBarFrame.height
+        let safeBottomArea: CGFloat = (statusBarH == 44 ? 34 : 0)
+        backgroundImageView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height + safeBottomArea)
+        safeAreaView.frame = CGRect(x: 0, y: bounds.height, width: bounds.width, height: safeBottomArea)
         effectView.frame = backgroundImageView.frame
     }
 }
 
 //MARK: - 交互
-public extension LZToolBar {
+extension LZToolBar {
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
         var touchBound = bounds
         touchBound.size.height = bounds.height + 34.0
